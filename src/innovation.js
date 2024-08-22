b081d5d5a18c4668aa7b9fdcbed2de9377d0bfd6
saffeine <49037076+saffeine@users.noreply.github.com>
2024-08-22 11:30:38 +0100

    Update innovation.js

--- src/innovation.js
+++ src/innovation.js
@@ -12,10 +12,26 @@ class Innovation {
         if(innovationId == -1){
             innovationId = this.neurons.length;
             this.neurons.push(new Neuron(neuronId));
-        } return this.neurons(innovationId);
+        } return this.neurons[innovationId];
     };
 
-    getSynapse(from, to, ){
+    getSynapse(innovationId = -1){
+        if(innovationId < 0 || innovationId >= this.synapses.length){
+            innovationId = -1;
+        }
 
+        if(innovationId == -1){
+            innovationId = this.neurons.length;
+            this.synapses.push(new this.Synapses())
+        } return this.synapses[innovationId];
+    };
+
+    getSynapseId(from, to){
+        for(let i=0; i<this.synapses.length; i++){
+            let synapse = this.synapses[i];
+            if(synapse.from != from){ continue; }
+            if(synapse.to != to){ continue; }
+            return synapse.innovationId;
+        } return -1;
     };
 }
